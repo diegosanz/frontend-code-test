d2e7a88ba270cf92a753681d03122f51f61fb236
diego sanz <diego2sanz@gmail.com>
2025-03-01 16:51:06 +0100

    Show selected boxes counter

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react";
 import React from "react";
 import { addBox } from "../stores/actions/addBox";
+import { SelectedBoxesCounter } from "./SelectedBoxesCounter";
 
 const Toolbar = observer(() => {
   return (
@@ -8,7 +9,7 @@ const Toolbar = observer(() => {
       <button onClick={() => addBox()}>Add Box</button>
       <button>Remove Box</button>
       <input type="color" />
-      <span>No boxes selected</span>
+      <SelectedBoxesCounter />
     </div>
   );
 });
